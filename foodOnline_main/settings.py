b9f30ed8baab20eb97c7ff424e6a3d66ada3b40b
Pradeep Kumar Baral <iampradeep.baral@gmail.com>
2025-05-06 23:39:18 +0530

    create user model and super user model in db

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -86,6 +87,8 @@ DATABASES = {
         }
     
 }
+
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Password validation
