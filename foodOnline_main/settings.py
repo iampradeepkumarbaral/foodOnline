d0e4f1a163ae4108141266370da05242ee0cd654
Pradeep Kumar Baral <iampradeep.baral@gmail.com>
2025-05-06 21:39:31 +0530

    postgres data base and templates added

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'foodOnline_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,9 +74,14 @@ WSGI_APPLICATION = 'foodOnline_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foodOnline_db',
+        'USER' : 'postgres',
+        'PASSWORD':'admin',   
+        'HOST' : 'localhost',
+        'PORT' : '5432',
+        }
+    
 }
 
 
@@ -114,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT  = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'foodOnline_main/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
